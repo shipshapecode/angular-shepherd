de8dc33dff80847f574bdba53c50418482ad5a4a
Tom <atombrot@gmail.com>
2019-04-01 10:08:07 +0200

    Changed example to include styles via angular.json instead of app.component

--- projects/shepherd-tester/src/app/app.component.ts
+++ projects/shepherd-tester/src/app/app.component.ts
@@ -2,11 +2,9 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: [
     './app.component.css',
-    '../../../../node_modules/shepherd.js/dist/css/shepherd-theme-default.css'
   ]
 })
 export class AppComponent {

ecdf31b182ab0f39c42d78eb8779e68c73887cc3
Robert Wagner <rwwagner90@gmail.com>
2020-02-26 08:42:57 -0500

    Bump deps, Shepherd v7

--- projects/shepherd-tester/src/app/data.ts
+++ projects/shepherd-tester/src/app/data.ts
@@ -23,10 +23,6 @@ export const defaultStepOptions = {
   scrollTo: true,
   cancelIcon: {
     enabled: true
-  },
-
-  tippyOptions: {
-    duration: 500
   }
 };
 
@@ -42,17 +38,17 @@ export const steps = [
     ],
     classes: 'custom-class-name-1 custom-class-name-2',
     id: 'intro',
-    title: 'Welcome to Ember Shepherd!',
+    title: 'Welcome to Angular Shepherd!',
     text: `
           <p>
-            Ember Shepherd is a JavaScript library for guiding users through your Ember app.
-            It is an Ember addon that wraps <a href="https://github.com/shipshapecode/shepherd">Shepherd</a>
-            and extends its functionality. Shepherd uses <a href="https://atomiks.github.io/tippyjs/">Tippy.js</a>,
+            Angular Shepherd is a JavaScript library for guiding users through your Angular app.
+            It is an Angular library that wraps <a href="https://github.com/shipshapecode/shepherd">Shepherd</a>
+            and extends its functionality. Shepherd uses <a href="https://popper.js.org/">Popper.js</a>,
             another open source library, to position all of its steps and enable entrance and exit animations.
           </p>
         
           <p>
-            Tippy makes sure your steps never end up off screen or cropped by an
+            Popper makes sure your steps never end up off screen or cropped by an
             overflow. Try resizing your browser to see what we mean.
           </p>`
   },
